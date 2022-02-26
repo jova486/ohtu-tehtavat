07713f5a943ced5a758467c799c6bf574d9ee843
Jorma Valjakka <jorma.valjakka@helsinki.fi>
2022-02-26 20:32:45 +0200

    Kahden eri tuotteen lisäämisen jälkeen ostoskorin hinta on sama kuin tuotteiden hintojen summa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -27,3 +27,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(leipa)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_palauttaa_ostoskorin_hinnan_oikein(self):
+        maito = Tuote('Maito', 3)
+        leipa = Tuote('Leipa', 6)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+
+        self.assertEqual(self.kori.hinta(), 9)
