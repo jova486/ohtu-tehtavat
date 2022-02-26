5d478d33724ac873f59d44157c71e517c188a05e
Jorma Valjakka <jorma.valjakka@helsinki.fi>
2022-02-26 19:57:49 +0200

    test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara git commit -m test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara git status

---
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,45 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        self.__ostokset = []
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+
+    def tavaroita_korissa(self):
+        maara = 0
+        for ostos in self.__ostokset:
+            maara += ostos.lukumaara()
+
+        return maara
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
+
+    def hinta(self):
+        hinta = 0
+        for ostos in self.__ostokset:
+            hinta += ostos.tuote.hinta()*ostos.lukumaara()
+
+        return hinta
+        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        for tuote in self.__ostokset:
+            if tuote._nimi == lisattava.nimi:
+                tuote.lukumaara +=1
+                return
+        self.__ostokset.append(Ostos(lisattava))
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
