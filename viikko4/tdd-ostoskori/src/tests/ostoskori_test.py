59381460d573ae8b92b0e50badbe08435480127c
Jorma Valjakka <jorma.valjakka@helsinki.fi>
2022-02-26 20:45:15 +0200

    test_kahden_saman_tuotteen_lisaamisen_jalkee_hinta_sama

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -42,3 +42,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkee_hinta_sama(self):
+        maito = Tuote('Maito', 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 6)
